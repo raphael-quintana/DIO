22d18d9c48ea6f7265fa0262499ed617fd7c3c10
raphael-quintana <rapha.quintana13@gmail.com>
2026-03-30 20:19:58 -0300

    Arrays in JS

---
+++ Formacao_Logica_de_Programacao/Vetores-e-Matrizes.js
@@ -0,0 +1,12 @@
+
+//Definindo Vetores
+let Pokemon = ["Pikachu" , "Charmander", "Bulbassaur"]
+console.log(Pokemon[0])
+
+let timePokemon = [
+  ["Pikachu", "M", 1],
+  ["Chamander", "F", 3]
+]
+
+console.log(" O pokemon " + timePokemon[1][0] + " é do sexo " +  timePokemon[1][1] + 
+" e está no nível " + timePokemon[1][2] )
