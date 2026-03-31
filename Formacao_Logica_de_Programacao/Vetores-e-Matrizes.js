ab4da5e59af61addce0402c3e932cfdb415a0bb4
raphael-quintana <rapha.quintana13@gmail.com>
2026-03-30 21:35:55 -0300

    Updating Arrays

--- Formacao_Logica_de_Programacao/Vetores-e-Matrizes.js
+++ Formacao_Logica_de_Programacao/Vetores-e-Matrizes.js
@@ -1,12 +1,18 @@
 
-//Definindo Vetores
-let Pokemon = ["Pikachu" , "Charmander", "Bulbassaur"]
-console.log(Pokemon[0])
+//Definindo Vetores=arrays, e definindo o index entre []
+let pokemon = ["Pikachu" , "Charmander", "Bulbassaur"]
+console.log(pokemon[0])
 
+
+//Usandop um  vetor dentro de outro vetor, temos uma matriz.
 let timePokemon = [
   ["Pikachu", "M", 1],
-  ["Chamander", "F", 3]
+  ["Charmander", "M", 3],
+  ["Bulbassaur", "M", 5]
 ]
 
-console.log(" O pokemon " + timePokemon[1][0] + " é do sexo " +  timePokemon[1][1] + 
-" e está no nível " + timePokemon[1][2] )
+//No caso de matrizes, temos 2 indexes para preencher nos []. Lembrando que é
+//sempre alocado como linha/coluna.
+
+console.log("O pokemon " + timePokemon[0][0] + " é do sexo " +  timePokemon[0][1] + 
+" e está no nível: " + timePokemon[0][2] )
