df4f91dcb8b68388a6170c5f40e70428f28fe7ea
raphael-quintana <rapha.quintana13@gmail.com>
2026-03-31 19:49:22 -0300

    Switch Cases

---
+++ Formacao_Logica_de_Programacao/Estruturas-condicionais.js
@@ -0,0 +1,23 @@
+let possuiOvos = true
+let itensComprados = ""
+
+if(possuiOvos){
+itensComprados = "Leite"
+}
+else{
+console.log("passou na sessão de congelados")
+itensComprados = "Lasanha congelada"
+}
+
+console.log("item comprado: " + itensComprados)
+
+//if,else if ,else
+let nivelDeFome =  1
+
+if(nivelDeFome === 1){
+console.log("pouca fome")
+}else if(nivelDeFome ===2){
+console.log("muita fome")
+}else{
+console.log("Você comeria mais que o pica pau")
+}
