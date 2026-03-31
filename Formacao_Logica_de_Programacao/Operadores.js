99d655bb5a2a0ceadd12701263fd950c2e934b24
raphael-quintana <rapha.quintana13@gmail.com>
2026-03-30 22:23:51 -0300

    Updating

---
+++ Formacao_Logica_de_Programacao/Operadores.js
@@ -0,0 +1,46 @@
+/*Operadores Aritméticos*/
+
+// sinal de + usado para concatenar string
+let idade = 30
+console.log("valor da minha variavel " + idade)
+
+// '+' realizando somas
+idade = 30 + 6
+console.log("operação de adição " + idade)
+
+// '-' realizando subtrações
+let primeiroNumero = 1023
+let secundoNumero = 23
+console.log(primeiroNumero - secundoNumero)
+
+// '*' realizando multiplicações
+let multiplicador = 4
+let multiplicando = 12
+let produto = multiplicador * multiplicando
+console.log("resultado da multiplicação é " + produto)
+
+multiplicador = 8
+produto = multiplicador * multiplicando
+console.log("resultado da multiplicação é " + produto)
+
+// '/' realizando divisões
+let notaDoMercado = 50
+let pessoasParaDividir = 2
+console.log("operação de divisão :" + notaDoMercado / pessoasParaDividir)
+
+// '%' pegando o resto de uma divisão
+let calculo = 10 % 3
+console.log("operação de módulo " + calculo)
+
+//Incremento e Decremento
+let contador = 1
+// '++' incremento
+contador++
+contador++
+
+// '--' decremento
+contador--
+contador--
+contador--
+
+console.log(contador)
