93e3f6521a266385e5cf505d53fda5304bb0da86
raphael-quintana <rapha.quintana13@gmail.com>
2026-04-01 18:50:56 -0300

    Functions Class

---
+++ Formacao_Logica_de_Programacao/Funcoes.js
@@ -0,0 +1,25 @@
+torrar()
+
+
+function torrar(){
+ console.log("torrando pão")
+ injetarPao()
+}
+
+function injetarPao(){
+	console.log("preparando para injetar o pão")
+    console.log("finalizado")
+}
+
+
+function fazerMágica() {
+  console.log("Abracadabra! Coisas mágicas aconteceram!");
+}
+
+
+function saudar(nome) {
+  console.log("Olá, " + nome + "! Como você está?");
+}
+
+saudar("Pedrinho"); // Vai dizer Olá, Pedrinho! Como você está?
+saudar("Mariana"); // Vai dizer Olá, Mariana! Como você está?
