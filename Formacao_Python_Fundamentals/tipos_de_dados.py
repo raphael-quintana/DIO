639cd1f33cf4ddf5851ba8c442fa10dc240a669b
raphael-quintana <rapha.quintana13@gmail.com>
2026-04-13 11:30:30 -0300

    Defining Constants in Python

---
+++ Formacao_Python_Fundamentals/tipos_de_dados.py
@@ -0,0 +1,7 @@
+print(type(10))
+
+print(type(0.50))
+
+print(type("pamonha"))
+
+print(type(True))
---
+++ Formacao_Python_Fundamentals/variaveis_e_constantes.py
@@ -0,0 +1,30 @@
+variavel = 10
+print(variavel)
+
+variavel = 25
+print(variavel)
+
+age, name = (35, "Raphael")
+
+print(f"\nMeu nome é {name}, e tenho {age} anos!")
+
+age = 35
+name = "Haluanu"
+
+print(f"\nMeu nome é {name}, e tenho {age} anos!")
+
+#Na convenção do Python, para definir uma constante é só escrever o nome
+#INTEIRO EM MAIUSCULO.
+
+DEBUG = True
+STATES = [
+    "SP",
+    "RJ",
+    "SC",
+]
+
+#Boas práticas Python
+
+#Snake_Case
+#Escolher nomes sugestivos
+#Nomes de constantes todo em maiúsculo
