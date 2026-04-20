bde6663cee8731c06f065c015095911293c07528
raphael-quintana <rapha.quintana13@gmail.com>
2026-04-20 20:13:56 -0300

    Dict Class

---
+++ Formacao_Python_Fundamentals/dicionarios.py
@@ -0,0 +1,34 @@
+#Explorando dicionários
+
+#Lembrando que dicionários são conjuntos de pares chave-valor. repŕesentado por
+# {}.
+
+pessoa = {"nome": "Raphael", "idade": 35}
+
+pessoa = dict(nome="Guino", idade=11)
+
+pessoa["telefone"] = "11-942514165"
+
+print(pessoa)
+
+#Para acessar os dados, podemos usar o nome da chave em colchetes[]
+
+dados = {"nome": "Raphael", "idade": 34, "telefone": "11942514165"}
+
+print(dados["nome"])
+print(dados["idade"])
+print(dados["telefone"])
+
+#Com a mesma sintaxe, podemos alterar os valores, indexando pelo nome da chave
+
+dados["idade"] = 11
+dados["nome"] = "Guino"
+print(dados)
+
+#Os dicionários também aceitam aninhamento, podendo armazenar um dicionário dentro
+#de outro dicionário. Com a única condição de que a chave sempre seja imutável.
+
+
+
+
+
