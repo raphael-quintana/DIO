70218dd5179b661dcb17686e7074e8a58561389d
raphael-quintana <rapha.quintana13@gmail.com>
2026-04-23 20:20:05 -0300

    Constructor and Destructors

---
+++ Formacao_Python_Fundamentals/construtuores_e_destrutores.py
@@ -0,0 +1,18 @@
+class Cachorro:
+    def __init__(self, nome, cor, acordado=True):
+        print("Inicializando a classe!")
+        self.nome = nome
+        self.cor = cor
+        self.acordado = acordado
+
+    def __del__(self):
+        print("Removendo a instância da classe")
+
+
+    def falar(self):
+        print("Au")
+
+
+d= Cachorro("Dog", "preto")
+d.falar()
+
