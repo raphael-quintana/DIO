ba2745d3f26d94521679a8a5523b84f138ffa2f9
raphael-quintana <rapha.quintana13@gmail.com>
2026-04-28 20:15:38 -0300

    Updating with properties class

--- Formacao_Python_Fundamentals/encapsulamento.py
+++ Formacao_Python_Fundamentals/encapsulamento.py
@@ -28,4 +28,52 @@ conta.sacar(300)
     
 print(conta.mostrar_saldo())
 
+#explorando properties no python
 
+class Foo:
+    def __init__(self, x=None):
+        self._x = x
+
+    @property #A property transforma o metodo em propriedade, assim podemos usar a sintaxe de atributo.
+    def x(self):
+        return self._x or 0
+    
+    @x.setter
+    def x(self, value):
+        _x = self._x or 0
+        _value = value or 0
+        self._x = _x + _value
+
+    @x.deleter
+    def x(self):
+        self._x = -1
+
+
+
+foo = Foo(10)
+print(foo.x)
+foo.x = 10
+print(foo.x)
+del foo.x
+print(foo.x)
+
+
+#Explorando properties in another example
+
+class Pessoa:
+    def __init__(self, nome, ano_nascimento):
+        self._nome = nome
+        self._ano_nascimento = ano_nascimento
+
+
+    @property
+    def nome(self):
+        return self._nome
+    
+    @property
+    def idade(self):
+        _ano_atual = 2025
+        return _ano_atual - self._ano_nascimento
+    
+pessoa = Pessoa("Recruta", 2015)
+print(f"Nome: {pessoa.nome} \tIdade: {pessoa.idade}")
