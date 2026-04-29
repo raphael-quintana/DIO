584ab1c11bf9bfa99c2db1c1415619a75182a0a2
raphael-quintana <rapha.quintana13@gmail.com>
2026-04-29 19:25:49 -0300

    staticmethods and classmethods

---
+++ Formacao_Python_Fundamentals/metodos_de_classe_vs_estaticos.py
@@ -0,0 +1,25 @@
+#explorando métodos de classe e métodos estáticos.
+
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    @classmethod
+    def criar_de_data_nascimento(cls, ano, mes, dia , nome):
+        idade = 2025 - ano
+        return cls(nome, idade)
+    
+    @staticmethod
+    def e_maior_idade(idade):
+        return idade >= 18
+    
+
+#p = Pessoa("Raphael", 34)
+#print(p.nome, p.idade)
+
+p = Pessoa.criar_de_data_nascimento(1991, 6, 21, "Raphael")
+print(p.nome, p.idade)
+
+print(Pessoa.e_maior_idade(18))
+print(Pessoa.e_maior_idade(7))
