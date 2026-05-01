85e36494cd5a9ceaee7a1df7e6309c6d6887bc49
raphael-quintana <rapha.quintana13@gmail.com>
2026-05-01 16:38:05 -0300

    Updating

--- Formacao_Python_Fundamentals/iteradores_e_geradores.py
+++ Formacao_Python_Fundamentals/iteradores_e_geradores.py
@@ -36,20 +36,30 @@ for i in MeuIterador(numeros=[28, 122, 983]):
 ##vez que ele for chamado.
 
 #Exemplo
-import requests
+#import requests
 
-def fetch_products(api_url, max_pages=100):
-    page = 1
-    while page <= max_pages:
-        response = requests.get(f"{api_url}?page={page}")
-        data = response.json()
-        for product in data["products"]:
-            yield product
-        if "next_page" not in data:
-            break
-        page +=1
+#def fetch_products(api_url, max_pages=100):
+#    page = 1
+#    while page <= max_pages:
+#        response = requests.get(f"{api_url}?page={page}")
+#        data = response.json()
+#        for product in data["products"]:
+#            yield product
+#        if "next_page" not in data:
+#            break
+#        page +=1
 
 
 #Uso do gerador
-for product in fetch_products("http://api.example.com/products"):
-    print(product["name"])
+#for product in fetch_products():
+#    print(product["name"])
+
+
+#exemplo2
+
+def meu_gerador(numeros: list[int]):
+    for numero in numeros:
+        yield numero * 2
+
+for i in meu_gerador(numeros=[1, 2, 3]):
+    print(i)
