d0a5ea785d62545e1b5ef43f1668c4191d787f58
raphael-quintana <rapha.quintana13@gmail.com>
2026-07-13 20:11:36 -0300

    Updating

---
+++ Formacao_Backend_Python/FerramentasChecagemEstiloPEP8.py
@@ -0,0 +1,16 @@
+#Para nos ajudar a seguir as recomendações da PEP8, podemos usar ferramentas
+#de checagem de estilo  como flake8. Essas ferramentas verificam nosso código e 
+#nos informam onde estamos desviando do guia de estilo.
+
+
+#pip install flake8
+#flake8 meu_codigo.py
+
+#Formatação autiomática de código com o Black.
+
+#Black é uma ferramenta de formatação de código Python que segue a filosofia
+#"formato único". Black reformata todo o seu arquivo em um estilo consistente,
+#simplificando a tarefa de manter o código em conformidade com a PEP8.
+
+#pip install black
+#black meu_codigo.py
